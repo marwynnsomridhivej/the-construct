7f18af0c825ffc45995670c1859def7df909c943
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-06-07 23:51:31 -0400

    new StatsInfo as container for player stats in each queue

--- statsmanager/stats.py
+++ statsmanager/stats.py
@@ -4,11 +4,13 @@ from typing import Dict, List, Union
 
 from base import WrapperBase
 from exceptions import *
+from queuemanager import QueueType
 
 __all__ = (
     "StatsWrapper",
     "StatsGuildContainer",
     "StatsSeason",
+    "StatsInfo",
     "StatsPlayer",
 )
 
@@ -119,21 +121,173 @@ class StatsGuildContainer(WrapperBase):
 
 
 class StatsSeason(WrapperBase):
+    # TODO: Rename to StatsSeason
     __slots__ = (
         "name",
         "start_timestamp",
         "end_timestamp",
-        "match_count",
         "archived",
-        "players",
+        "r6_5v5",
+        "r6_1v1",
     )
+
+    # Used to convert enum text to class attribute name
+    __QT_ATTR_CONVERSION = {
+        QueueType.R6_5V5: "r6_5v5",
+        QueueType.R6_1V1: "r6_1v1",
+    }
 
     def __init__(self, data: dict):
         self.name: str = data["name"]
         self.start_timestamp: int = data["start_timestamp"]
         self.end_timestamp: int = data.get("end_timestamp", None)
-        self.match_count: int = data["match_count"]
         self.archived: bool = data["archived"]
+
+        self.r6_5v5: StatsInfo = StatsInfo.parse(
+            data[self.__QT_ATTR_CONVERSION[QueueType.R6_5V5]])
+        self.r6_1v1: StatsInfo = StatsInfo.parse(
+            data[self.__QT_ATTR_CONVERSION[QueueType.R6_1V1]])
+
+    @property
+    def is_current(self) -> bool:
+        return not self.archived
+
+    def get_data_by_queue_type(self, queue_type: QueueType) -> "StatsInfo":
+        data = getattr(self, self.__QT_ATTR_CONVERSION[queue_type])
+        assert isinstance(data, StatsInfo)
+        return data
+
+    def stop_season(self) -> None:
+        self.end_timestamp = int(datetime.now().timestamp())
+        self.archived = True
+
+    def get_player(self, queue_type: QueueType, user_id: int, throw: bool = False) -> Union["StatsPlayer", None]:
+        """Get a StatsPlayer in the specified queue type with the specified user ID
+
+        Args:
+            queue_type (QueueType): The queue type to search
+            user_id (int): The user ID of the desired StatsPlayer instance
+            throw (bool): Whether or not to throw an exception if a StatsPlayer instance cannot be found
+
+        Raises:
+            PlayerDoesNotExist: No StatsPlayer instance exists in the specified queue type with the specified user ID
+
+        Returns:
+            Union[StatsPlayer, None]: The StatsPlayer instance, if found
+        """
+        data = self.get_data_by_queue_type(queue_type)
+        player = data.players.get(user_id)
+        if player is None and throw:
+            raise PlayerDoesNotExist(user_id)
+        return player
+
+    def create_player(self, queue_type: QueueType, user_id: int) -> "StatsPlayer":
+        """Create a StatsPlayer in the specified queue type with the specified user ID
+
+        Args:
+            queue_type (QueueType): The queue type to search
+            user_id (int): The ID of the user
+
+        Raises:
+            PlayerAlreadyExists: A StatsPlayer already exists for the specified user
+
+        Returns:
+            StatsPlayer: A newly created StatsPlayer instance for the specified user in the specified queue type
+        """
+        data = self.get_data_by_queue_type(queue_type)
+        if data.players.get(user_id) is not None:
+            raise PlayerAlreadyExists(user_id)
+        data.players[user_id] = StatsPlayer.create_zeroed(user_id)
+        return data.players[user_id]
+
+    def delete_player(self, queue_type: QueueType, user_id: int) -> None:
+        """Deletes a StatsPlayer entry in the specified queue type for the specified user ID
+
+        Args:
+            queue_type (QueueType): The queue type to search
+            user_id (int): The ID of the user
+
+        Raises:
+            PlayerDoesNotExist: No StatsPlayer exists in the specified queue type for the specified user ID
+        """
+        data = self.get_data_by_queue_type(queue_type)
+        if data.players.get(user_id) is None:
+            raise PlayerDoesNotExist(user_id)
+        del data.players[user_id]
+
+    def edit_player(self, queue_type: QueueType, user_id: int, new_stats: dict) -> "StatsPlayer":
+        """Edits a player's stats entry
+
+        Args:
+            queue_type (QueueType): The queue type to search
+            user_id (int): The ID of the user
+            new_stats (dict): A payload containing user stats
+
+        Raises:
+            PlayerDoesNotExist: No StatsPlayer exists for the specified user
+
+        Returns:
+            StatsPlayer: The edited StatsPlayer instance
+        """
+        data = self.get_data_by_queue_type(queue_type)
+        if data.players.get(user_id) is None:
+            raise PlayerDoesNotExist(user_id)
+
+        player = data.players[user_id]
+        player.wins = new_stats["wins"]
+        player.losses = new_stats["losses"]
+        player.points = new_stats["points"]
+        player.max_points = new_stats["max_points"]
+        player.times_mvp = new_stats["times_mvp"]
+        return player
+
+    def award_player(self, queue_type: QueueType, user_id: int, mvp_id: int, win: bool) -> None:
+        """Awards the specified player points for winning or losing a match
+
+        Args:
+            queue_type (QueueType): The queue type to search
+            user_id (int): The ID of the user
+            mvp_id (int): The ID of the team's MVP
+            win (bool): Whether or not the player was on the winning team
+        """
+        try:
+            player = self.get_player(queue_type, user_id, throw=True)
+        except PlayerDoesNotExist:
+            player = self.create_player(queue_type, user_id)
+
+        func = player.win if win else player.lose
+        func(user_id == mvp_id, queue_type)
+
+    def serialise(self) -> dict:
+        return {
+            "name": self.name,
+            "start_timestamp": self.start_timestamp,
+            "end_timestamp": self.end_timestamp,
+            "archived": self.archived,
+            "r6_5v5": self.r6_5v5.serialise(),
+            "r6_1v1": self.r6_1v1.serialise(),
+        }
+
+    @classmethod
+    def create_blank(cls, name: str) -> "StatsSeason":
+        return cls({
+            "name": name,
+            "start_timestamp": int(datetime.now().timestamp()),
+            "end_timestamp": None,
+            "archived": False,
+            "r6_5v5": StatsInfo.create_blank().serialise(),
+            "r6_1v1": StatsInfo.create_blank().serialise(),
+        })
+
+
+class StatsInfo(WrapperBase):
+    __slots__ = (
+        "match_count",
+        "players",
+    )
+
+    def __init__(self, data: dict):
+        self.match_count: int = data["match_count"]
 
         assert isinstance(data["players"], dict)
         self.players: Dict[int, StatsPlayer] = {
@@ -144,125 +298,23 @@ class StatsSeason(WrapperBase):
     def player_count(self) -> int:
         return len(self.players.keys()) if self.players else 0
 
-    @property
-    def is_current(self) -> bool:
-        return not self.archived
-
-    def stop_season(self) -> None:
-        self.end_timestamp = int(datetime.now().timestamp())
-        self.archived = True
-
-    def get_player(self, user_id: int, throw: bool = False) -> Union["StatsPlayer", None]:
-        """Get a StatsPlayer with the specified user ID
-
-        Args:
-            user_id (int): The user ID of the desired StatsPlayer instance
-            throw (bool): Whether or not to throw an exception if a StatsPlayer instance is not found
-
-        Raises:
-            PlayerDoesNotExist: No StatsPlayer instance exists with the specified user ID
-
-        Returns:
-            Union[StatsPlayer, None]: The StatsPlayer instance with the specified user ID
-        """
-        data = self.players.get(user_id)
-        if data is None and throw:
-            raise PlayerDoesNotExist(user_id)
-        return data
-
-    def create_player(self, user_id: int) -> "StatsPlayer":
-        """Create a StatsPlayer with specified user_id
-
-        Args:
-            user_id (int): The ID of the user
-
-        Raises:
-            PlayerAlreadyExists: A StatsPlayer already exists for the specified user
-
-        Returns:
-            StatsPlayer: The newly created StatsPlayer instance for the specified user
-        """
-        if self.players.get(user_id) is not None:
-            raise PlayerAlreadyExists(user_id)
-        self.players[user_id] = StatsPlayer.create_zeroed(user_id)
-        return self.players[user_id]
-
-    def delete_player(self, user_id: int) -> None:
-        """Deletes a player's stats entry entirely (NOT RESET)
-
-        Args:
-            user_id (int): The ID of the user
-
-        Raises:
-            PlayerDoesNotExist: No StatsPlayer exists for the specified user
-        """
-        if self.players.get(user_id) is None:
-            raise PlayerDoesNotExist(user_id)
-        del self.players[user_id]
-
-    def edit_player(self, user_id: int, new_stats: dict) -> "StatsPlayer":
-        """Edits a player's stats entry
-
-        Args:
-            user_id (int): The ID of the user
-            new_stats (dict): A payload containing user stats
-
-        Raises:
-            PlayerDoesNotExist: No StatsPlayer exists for the specified user
-        """
-        if self.players.get(user_id) is None:
-            raise PlayerDoesNotExist
-
-        player = self.players[user_id]
-        player.wins = new_stats["wins"]
-        player.losses = new_stats["losses"]
-        player.points = new_stats["points"]
-        player.max_points = new_stats["max_points"]
-        player.times_mvp = new_stats["times_mvp"]
-        return player
-
-    def award_player(self, user_id: int, mvp_id: int, win: bool, is_1v1: bool) -> None:
-        """Awards the specified player points for winning or losing a match
-
-        Args:
-            user_id (int): The ID of the player
-            mvp_id (int): The ID of the team's MVP
-            win (bool): Whether or not the player was on the winning team
-            is_1v1 (bool): Whether or not the match played was a 1v1
-        """
-        try:
-            player = self.get_player(user_id, throw=True)
-        except PlayerDoesNotExist:
-            player = self.create_player(user_id)
-
-        func = player.win if win else player.lose
-        func(user_id == mvp_id, is_1v1)
-
     def serialise(self) -> dict:
-        """Convert StatsSeason instance representation into a dict
+        """Convert StatsInfo instance representation into a dict
 
         Returns:
-            dict: Dictionary representation of the StatsSeason instance
+            dict: Dictionary representation of the StatsInfo instance
         """
         return {
-            "name": self.name,
-            "start_timestamp": self.start_timestamp,
-            "end_timestamp": self.end_timestamp,
             "match_count": self.match_count,
-            "archived": self.archived,
             "players": {
                 user_id: player.serialise() for user_id, player in self.players.items()
-            }
+            },
         }
 
     @classmethod
-    def create_blank(cls, name: str) -> "StatsSeason":
+    def create_blank(cls) -> "StatsInfo":
         return cls({
-            "name": name,
-            "start_timestamp": int(datetime.now().timestamp()),
-            "end_timestamp": None,
             "match_count": 0,
-            "archived": False,
             "players": {}
         })
 
@@ -285,17 +337,30 @@ class StatsPlayer(WrapperBase):
         self.points: int = data["points"]
         self.max_points: int = data["max_points"]
 
-    def win(self, mvp, is_1v1: bool) -> None:
+    def __eq__(self, other: "StatsPlayer") -> bool:
+        return all([getattr(self, attr) == getattr(other, attr) for attr in [
+            "id",
+            "wins",
+            "losses",
+            "times_mvp",
+            "points",
+            "max_points",
+        ]])
+
+    def __ne__(self, other: "StatsPlayer") -> bool:
+        return not self == other
+
+    def win(self, mvp: bool, queue_type: QueueType) -> None:
         """Awards a win to the player and adjusts values accordingly
 
         Args:
             mvp (bool): Whether or not the player was the team MVP
-            is_1v1 (bool): Whether or not the match played was a 1v1
+            queue_type (QueueType): The queue type of the match
         """
         self.wins += 1
 
         # Default point gain per win (+1 for 1v1, +2 otherwise)
-        self.points += 1 if is_1v1 else 2
+        self.points += 1 if queue_type == QueueType.R6_1V1 else 2
 
         if mvp:
             # 1 bonus point, and MVP designation
@@ -305,16 +370,15 @@ class StatsPlayer(WrapperBase):
         if self.points > self.max_points:
             self.max_points = self.points
 
-    def lose(self, mvp: bool, is_1v1) -> None:
+    def lose(self, mvp: bool, *_) -> None:
         """Awards a loss to the player and adjusts values accordingly
 
         Args:
             mvp (bool): Whether or not the player was the team MVP
-            is_1v1 (bool): Whether or not the match played was a 1v1
         """
         self.losses += 1
 
-        # Default point loss per loss (1v1 does not affect point loss)
+        # Default point loss per loss
         self.points -= 1
 
         if mvp:
