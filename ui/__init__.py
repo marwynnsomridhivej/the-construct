62c250748e719a9ba547ef5d0ff1f8344f0d5125
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-06-19 00:52:05 -0400

    just import everything and don't keep an __all__

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,24 +1,3 @@
-__all__ = (
-    # Modals
-    "FeedbackModal",
-
-    "PreMatchModal",
-
-    "R6DraftModal",
-    "R6MapBanModal",
-    "R6MVPModal",
-
-    # Views
-    "R6View",
-    "MatchStartDMView",
-
-    # Util
-    "R6URL",
-    "FeedbackType",
-    "FEEDBACK_TYPES",
-)
-
-
 from .feedback_type import *
 from .modals import *
 from .urls import *
