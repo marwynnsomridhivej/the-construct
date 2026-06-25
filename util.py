4ca521a8c8848e8dddbee658e839168c3c010223
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-06-24 23:57:27 -0400

    add ICON constant to replace old R6URL enum

--- util.py
+++ util.py
@@ -1,6 +1,14 @@
-__all__ = ("ephemeral",)
+__all__ = (
+    "ICON",
+    "ephemeral",
+)
 
 
+# Constants
+ICON = "https://img.icons8.com/ios_filled/1200/rainbow-six-siege.jpg"
+
+
+# Util functions
 def ephemeral(*, seconds: int = 5) -> dict:
     return {
         "ephemeral": True,
