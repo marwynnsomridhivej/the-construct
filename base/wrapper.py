c12bf3cb2a8d85930bb5f06e2979380c893f5760
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-06-28 13:09:33 +0700

    implement type hinting improvements from ManagerBase

--- base/wrapper.py
+++ base/wrapper.py
@@ -1,21 +1,32 @@
 from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, TypeVar
 
-__all__ = ("WrapperBase",)
+__all__ = (
+    "WrapperBase",
+    "WrapperBaseType",
+)
+
+if TYPE_CHECKING:
+    WrapperBaseType = TypeVar("WrapperBaseType", bound="WrapperBase")
 
 
 class WrapperBase(ABC):
+    def __init__(self, *args, **kwargs):
+        pass
+
     @abstractmethod
     def serialise(self) -> dict:
         raise NotImplementedError
 
     @classmethod
-    def parse(cls, data: dict):
+    def parse(cls: type[WrapperBaseType], data: dict) -> WrapperBaseType:
         """Classmethod to create an instance of the wrapper class from dict representation of data
 
         Args:
+            cls (type[WrapperBaseType]): The wrapper class instance
             data (dict): Dictionary representation of class data
 
         Returns:
-            Type[WrapperBase]: Wrapper class instance that inherits from WrapperBase
+            WrapperBaseType: Wrapper class instance that inherits from WrapperBase
         """
         return cls(data)
