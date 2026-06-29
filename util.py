ba6e637b3fc92482e7ebc81536e3bf8cc77e5cee
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-06-29 10:22:12 +0700

    move commonly used event handler typehints here to deduplicate code

--- util.py
+++ util.py
@@ -1,6 +1,27 @@
+from collections.abc import Callable, Coroutine
+from typing import TYPE_CHECKING, Any
+
+import discord
+
+if TYPE_CHECKING:
+    from event import (
+        DMDeletePayload,
+        MatchFinalisedPayload,
+        MatchPayload,
+        PlayerStatsResetPayload,
+        PrematchDMPayload,
+        SeasonEndPayload,
+        VCResetPayload,
+    )
+
 __all__ = (
+    # Constants
     "ICON",
+    # Functions
     "ephemeral",
+    # Typehints
+    "CoroType",
+    "EventHandlerType",
 )
 
 
@@ -14,3 +35,20 @@ def ephemeral(*, seconds: int = 5) -> dict:
         "ephemeral": True,
         "delete_after": seconds,
     }
+
+
+# Typehints
+type CoroType = Coroutine[Any, Any, None]
+type EventHandlerType = (
+    Callable[[discord.Guild], CoroType]
+    | Callable[[discord.RawMemberRemoveEvent], CoroType]
+    | Callable[[discord.RawMessageDeleteEvent], CoroType]
+    | Callable[[DMDeletePayload], CoroType]
+    | Callable[[int], CoroType]
+    | Callable[[MatchFinalisedPayload], CoroType]
+    | Callable[[MatchPayload], CoroType]
+    | Callable[[PlayerStatsResetPayload], CoroType]
+    | Callable[[PrematchDMPayload], CoroType]
+    | Callable[[SeasonEndPayload], CoroType]
+    | Callable[[VCResetPayload], CoroType]
+)
