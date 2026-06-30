5ebd529f321a3c5416d828a63ef14ebd02edc3d8
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-06-30 19:05:13 +0700

    add queue modals to ui package

--- ui/modals/__init__.py
+++ ui/modals/__init__.py
@@ -1,5 +1,6 @@
 from .confirmation import *  # noqa: F403
 from .feedback import *  # noqa: F403
 from .match import *  # noqa: F403
+from .queue import *  # noqa: F403
 from .season_start import *  # noqa: F403
 from .settings import *  # noqa: F403
