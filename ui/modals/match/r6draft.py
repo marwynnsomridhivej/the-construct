4cc9e4627c6f223bc43c3a946d02c82d9aaba06e
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-07-03 12:44:01 +0700

    add ability to finalised your own team's draft early and give the rest of the players in the pool to the other team

--- ui/modals/match/r6draft.py
+++ ui/modals/match/r6draft.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 import discord
 
 from canned import Canned
+from matchmanager import R6_MAX_PLAYERS_PER_TEAM, MatchTeam
 
 if TYPE_CHECKING:
     from ...views import R6View
@@ -14,14 +15,30 @@ __all__ = ("R6DraftModal",)
 
 
 class R6DraftModal(discord.ui.Modal):
-    def __init__(self, *, view):
+    def __init__(self, *, view: R6View, other_team: MatchTeam):
         super().__init__(title="Draft Player")
-        self.r6view: R6View = view
+        self.r6view = view
+        self.other_team = other_team
 
         for item in self.init_components():
             self.add_item(item)
 
-    def init_components(self) -> list[discord.ui.Item]:
+    @property
+    def can_show_finalised_button(self) -> bool:
+        return (
+            R6_MAX_PLAYERS_PER_TEAM - len(self.other_team.players)
+            >= len(self.r6view.draftable_players) - 1
+        )
+
+    @property
+    def team_draft_finalised(self) -> bool:
+        assert isinstance(self.mark_final_draft.component, discord.ui.Select)
+        return (
+            bool(self.mark_final_draft.component.values)
+            and self.mark_final_draft.component.values[0] == "yes"
+        )
+
+    def init_components(self) -> list[discord.ui.Label]:
         self.draft = discord.ui.Label(
             text="Draft Player",
             description="Select a player to draft",
@@ -33,26 +50,81 @@ class R6DraftModal(discord.ui.Modal):
                 required=True,
             ),
         )
-        return [self.draft]
+        self.mark_final_draft = discord.ui.Label(
+            text="Finalise Team Draft",
+            description="Would you like to give the other team the rest of the players (except for the one you selected)?",
+            component=discord.ui.Select(
+                options=[
+                    discord.SelectOption(
+                        label=opt.title(), value=opt, default=opt == "no"
+                    )
+                    for opt in ["yes", "no"]
+                ],
+                min_values=1,
+                max_values=1,
+                required=True,
+            ),
+        )
+
+        items = [self.draft]
+        if self.can_show_finalised_button:
+            items.append(self.mark_final_draft)
+
+        return items
 
     async def on_submit(self, interaction: discord.Interaction):
         assert isinstance(self.draft.component, discord.ui.RadioGroup)
+        assert isinstance(self.mark_final_draft.component, discord.ui.Select)
 
         captain_id = interaction.user.id
         drafted_id = int(self.draft.component.value)
+        remaining_player_ids = remaining_player_ids = [
+            int(player_id)
+            for _, player_id in self.r6view.draftable_players
+            if int(player_id) != drafted_id
+        ]
 
         # Use MatchManager.draft to write to disk
         await self.r6view.bot.match_manager.draft_player(
             interaction.guild_id, self.r6view.payload.match_name, captain_id, drafted_id
         )
 
+        # If finalise draft is selected, draft all remaining players in the pool to the other team
+        if self.team_draft_finalised:
+            for player_id in remaining_player_ids:
+                await self.r6view.bot.match_manager.draft_player(
+                    interaction.guild_id,
+                    self.r6view.payload.match_name,
+                    self.other_team.captain_id,
+                    player_id,
+                )
+
         # Update local MatchEntry instance attached to R6View
         await self.r6view.update_match()
 
         # Notify
-        await interaction.response.send_message(
-            f"Captain <@{captain_id}> has drafted <@{drafted_id}>", delete_after=10.0
-        )
+        msg = [
+            f"Captain <@{captain_id}> has drafted <@{drafted_id}>",
+        ]
+
+        # If team draft finalised was selected, add to notification
+        if self.team_draft_finalised:
+            msg.append(
+                "\n".join(
+                    [
+                        f"\n**Captain <@{captain_id}> finalised their team draft early**. The following "
+                        + f"players have been drafted captain <@{self.other_team.captain_id}>'s team:",
+                        "\n".join([f"- <@{_id}>" for _id in remaining_player_ids]),
+                    ]
+                )
+            )
+
+        # Send draft notificatoin
+        await interaction.response.send_message("\n".join(msg), delete_after=10)
+
+        # If team draft finalised, return early (no need to check if players remain in the pool)
+        if self.team_draft_finalised:
+            return
 
         # Check if there is one player left. If so, do the following actions:
         # If playercount is:
