162b19380b4c2f075a53c5dad97af0a394a7e7c2
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-07-05 10:42:24 +0700

    typehinting assertions and make it so that the modal is never valid by default

--- ui/modals/settings/settings_general.py
+++ ui/modals/settings/settings_general.py
@@ -21,9 +21,9 @@ class SettingsSetAdminModal(discord.ui.Modal):
     def __init__(self, bot: Bot, previous: list[int]):
         super().__init__(title="Set Admins")
         self.previous = previous
-        self.__bot = bot
-        self.admins: list[int] = None
-        self.is_valid = True
+        self.bot = bot
+        self.admins: list[int]
+        self.is_valid = False
 
         self.init_components()
 
@@ -36,7 +36,9 @@ class SettingsSetAdminModal(discord.ui.Modal):
                 min_values=0,
                 max_values=5,
                 default_values=[
-                    self.__bot.get_user(user_id) for user_id in self.previous
+                    user
+                    for user_id in self.previous
+                    if (user := self.bot.get_user(user_id))
                 ],
                 required=False,
             ),
@@ -45,47 +47,56 @@ class SettingsSetAdminModal(discord.ui.Modal):
 
     async def on_submit(self, interaction: discord.Interaction):
         assert isinstance(self.admin_select.component, discord.ui.UserSelect)
+
+        msg = None
+
         if any([user.bot for user in self.admin_select.component.values]):
-            self.is_valid = False
+            msg = Canned.ERR_SETTINGS_SET_ADMIN_BOT_USER
         else:
             self.admins = [user.id for user in self.admin_select.component.values]
 
         if sorted(self.admins) == sorted(self.previous):
-            self.is_valid = False
+            msg = Canned.SETTINGS_SET_ADMIN_ALREADY_SET
 
+        if msg is not None:
+            await interaction.response.send_message(msg, **ephemeral())
+            return
+
+        self.is_valid = True
         await interaction.response.defer()
         self.stop()
 
     async def on_error(self, interaction: discord.Interaction, error: Exception):
-        self.__bot.logger.error(f"An exception occurred when select users: {error}")
+        self.bot.logger.error(f"An exception occurred when select users: {error}")
         traceback.print_exception(type(error), error, error.__traceback__)
 
         await interaction.response.send_message(
             Canned.ERR_SETTINGS_SET_ADMIN, **ephemeral()
         )
-        self.is_valid = False
         self.stop()
 
 
 class SettingsBindTextChannelModal(discord.ui.Modal):
-    def __init__(self, bot, previous: int):
+    def __init__(self, bot: Bot, previous: int):
         super().__init__(title="Bind Text Channel")
         self.previous = previous
-        self.__bot: Bot = bot
-        self.channel_id: int = None
-        self.is_valid = True
+        self.bot = bot
+        self.channel_id: int
+        self.is_valid = False
 
         self.init_components()
 
     def init_components(self) -> None:
+        default_text_channel = self.bot.get_channel(self.previous)
+
         self.channel_select = discord.ui.Label(
             text="Select Text Channel",
             description="Please select the text channel all match related threads should be created in.",
             component=discord.ui.ChannelSelect(
                 channel_types=[discord.ChannelType.text],
                 placeholder="Select a text channel",
-                default_values=[self.__bot.get_channel(self.previous)]
-                if isinstance(self.previous, int)
+                default_values=[default_text_channel]
+                if isinstance(default_text_channel, discord.TextChannel)
                 else discord.utils.MISSING,
                 required=True,
             ),
@@ -95,29 +106,33 @@ class SettingsBindTextChannelModal(discord.ui.Modal):
     async def on_submit(self, interaction: discord.Interaction):
         assert isinstance(self.channel_select.component, discord.ui.ChannelSelect)
         channel = self.channel_select.component.values[0]
-        if self.previous == channel.id:
-            self.is_valid = False
 
-        if (
-            not channel.resolve()
-            .permissions_for(channel.guild.get_member(self.__bot.user.id))
-            .create_private_threads
-        ):
-            self.is_valid = False
-            return await interaction.response.send_message(
-                Canned.ERR_SETTINGS_BIND_CHANNEL_PERMS, **ephemeral()
-            )
+        assert (guild := interaction.guild) is not None
+        assert (resolved_channel := channel.resolve()) is not None
+        assert self.bot.user is not None
+        assert (bot_member := guild.get_member(self.bot.user.id)) is not None
+
+        msg = None
+
+        if self.previous == channel.id:
+            msg = Canned.SETTINGS_BIND_CHANNEL_ALREADY_BOUND
+        elif not resolved_channel.permissions_for(bot_member).create_private_threads:
+            msg = Canned.ERR_SETTINGS_BIND_CHANNEL_PERMS
+
+        if msg is not None:
+            await interaction.response.send_message(msg, **ephemeral())
+            return
 
         self.channel_id = channel.id
+        self.is_valid = True
         await interaction.response.defer()
         self.stop()
 
     async def on_error(self, interaction: discord.Interaction, error: Exception):
-        self.__bot.logger.error(f"An exception occurred when select users: {error}")
+        self.bot.logger.error(f"An exception occurred when select users: {error}")
         traceback.print_exception(type(error), error, error.__traceback__)
 
         await interaction.response.send_message(
             Canned.ERR_SETTINGS_BIND_CHANNEL, **ephemeral()
         )
-        self.is_valid = False
         self.stop()
