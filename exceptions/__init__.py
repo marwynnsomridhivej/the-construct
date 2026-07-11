4aaf114b0175a20fe4a89e064f7826d778d70530
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-07-11 10:41:43 +0700

    remove unused modal exception classes and imports

--- exceptions/__init__.py
+++ exceptions/__init__.py
@@ -1,6 +1,5 @@
 from .generic import *  # noqa: F403
 from .match import *  # noqa: F403
-from .modal import *  # noqa: F403
 from .queue import *  # noqa: F403
 from .settings import *  # noqa: F403
 from .stats import *  # noqa: F403
