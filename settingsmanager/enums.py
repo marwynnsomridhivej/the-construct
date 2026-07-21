b6a76d2b75973ccb830d5ab68fb9adb5608d126c
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-07-21 09:14:28 -0400

    docstring for all classes and constants

--- settingsmanager/enums.py
+++ settingsmanager/enums.py
@@ -11,11 +11,15 @@ __all__ = (
 
 
 class SettingsChoice(StrEnum):
+    """Standard text representation of settings command choices."""
+
     GENERAL = "general"
     MAP_POOL = "map pool"
 
 
 class MapPoolName(StrEnum):
+    """Standard text representation of default map pool names."""
+
     RANKED = "default - ranked"
     QUICKMATCH = "default - quickmatch"
 
@@ -25,12 +29,19 @@ ALL_SETTINGS_CHOICES = [
     SettingsChoice.GENERAL,
     SettingsChoice.MAP_POOL,
 ]
+"""All settings choices."""
 
 
 DEFAULT_MAP_POOL_NAMES = [
     MapPoolName.RANKED,
     MapPoolName.QUICKMATCH,
 ]
+"""All default map pool names."""
+
 
 PER_GUILD_MAP_POOL_LIMIT = 10
+"""The maximum number of custom map pools a guild can have."""
+
+
 PER_MAP_POOL_LIMIT = 20
+"""The maximum number of maps a custom map pool can contain."""
