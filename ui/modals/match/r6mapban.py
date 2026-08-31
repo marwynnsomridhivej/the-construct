99d7e158b2d2e489dfcd8d8f642ac5f4af9fffe8
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-08-30 20:52:37 -0400

    add gentleman's agreement selector and handle selection upon reaching an agreement

--- ui/modals/match/r6mapban.py
+++ ui/modals/match/r6mapban.py
@@ -8,7 +8,7 @@ import discord
 
 from canned import Canned
 from exceptions import MatchPanelStateException
-from matchmanager import R6Map
+from matchmanager import R6_QUICKMATCH, R6Map
 from util import ephemeral, titlecase
 
 if TYPE_CHECKING:
@@ -43,10 +43,29 @@ class R6MapBanModal(discord.ui.Modal):
                 required=False,
             ),
         )
-        return [self.map_ban]
+
+        self.gentleman = discord.ui.Label(
+            text="Gentleman's Agreement",
+            description="If both teams select the same map in this field, it "
+            + "will be the map this match is played on.",
+            component=discord.ui.Select(
+                options=[
+                    discord.SelectOption(
+                        label=titlecase(r6map.replace("_", " ")),
+                        value=r6map.value,
+                    )
+                    for r6map in R6_QUICKMATCH
+                ],
+                min_values=0,
+                max_values=1,
+                required=False,
+            ),
+        )
+        return [self.map_ban, self.gentleman]
 
     async def on_submit(self, interaction: discord.Interaction):
         assert isinstance(self.map_ban.component, discord.ui.CheckboxGroup)
+        assert isinstance(self.gentleman.component, discord.ui.Select)
         assert isinstance(interaction.channel, discord.Thread)
         assert interaction.guild_id is not None
 
@@ -57,8 +76,11 @@ class R6MapBanModal(discord.ui.Modal):
 
         captain_id = interaction.user.id
         maps_banned = {R6Map(map_name) for map_name in self.map_ban.component.values}
+        gentleman_agreement = [
+            R6Map(map_name) for map_name in self.gentleman.component.values
+        ]
 
-        # Use MatchManager.ban_map to write any banned maps to disk
+        # Use MatchManager.ban_maps to write any banned maps to disk
         if maps_banned:
             await self.r6view.bot.match_manager.ban_maps(
                 interaction.guild_id,
@@ -67,12 +89,22 @@ class R6MapBanModal(discord.ui.Modal):
                 maps_banned,
             )
 
+        # Use MatchManager.add_gentleman to write a team's selected gentleman's
+        # agreement map to disk
+        if gentleman_agreement:
+            await self.r6view.bot.match_manager.add_gentleman(
+                interaction.guild_id,
+                self.r6view.payload.match_name,
+                captain_id,
+                gentleman_agreement[0],
+            )
+
         # Add captain ID to list of captains that have submitted map bans
         self.r6view.map_bans_locked_captain_ids.append(captain_id)
 
         # Notify that the captain has completed map bans
         await interaction.response.send_message(
-            f"Captain <@{captain_id}> has completed map bans.", delete_after=10.0
+            f"Captain <@{captain_id}> has completed map bans."
         )
 
         # Update local MatchEntry instance attached to R6View
@@ -80,15 +112,18 @@ class R6MapBanModal(discord.ui.Modal):
 
         # Detect if 4 bans were done in total (each side completed their two bans)
         if len(self.r6view.map_bans_locked_captain_ids) == 2:
-            maps_remaining = [
-                _map
-                for _map in self.r6view.map_pool
-                if _map not in self.r6view.match.banned_maps
-            ]
+            # Check if any gentleman's agreement has been reached
+            chosen_map = self.r6view.match.gentleman_map or secrets.choice(
+                [
+                    _map
+                    for _map in self.r6view.map_pool
+                    if _map not in self.r6view.match.banned_maps
+                ]
+            )
             await self.r6view.bot.match_manager.select_map(
                 interaction.guild_id,
                 self.r6view.payload.match_name,
-                secrets.choice(maps_remaining),
+                chosen_map,
             )
 
             # Need to update local MatchEntry instance again
