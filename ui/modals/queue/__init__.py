1cca6eab94742416be880c4b6eeb3343f678cf42
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-09-09 01:09:37 -0400

    add queue invite, and notify modals, remove unnecessary noqa F403

--- ui/modals/queue/__init__.py
+++ ui/modals/queue/__init__.py
@@ -1,6 +1,8 @@
-from .queue_create_modal import *  # noqa: F403
-from .queue_delete_modal import *  # noqa: F403
-from .queue_join_modal import *  # noqa: F403
-from .queue_leave_modal import *  # noqa: F403
-from .queue_lock_modal import *  # noqa: F403
-from .queue_unlock_modal import *  # noqa: F403
+from .queue_create_modal import *
+from .queue_delete_modal import *
+from .queue_invite_modal import *
+from .queue_join_modal import *
+from .queue_leave_modal import *
+from .queue_lock_modal import *
+from .queue_notify_modal import *
+from .queue_unlock_modal import *
