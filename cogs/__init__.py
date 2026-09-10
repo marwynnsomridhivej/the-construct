17343832b56211e25035f8188c9971c912ef34da
Marwynn Somridhivej <msomridhivej329@gmail.com>
2026-09-10 11:54:31 -0400

    added all up to date cog information

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,10 +1,22 @@
 __all__ = (
+    "HelpCog",
     "MatchCog",
+    "MonitoringCog",
+    "PlayerCog",
     "QueueCog",
+    "ReportsCog",
+    "SeasonCog",
+    "SettingsCog",
     "StatsCog",
 )
 
 
+from .help_cog import HelpCog
 from .match_cog import MatchCog
+from .monitoring_cog import MonitoringCog
+from .player_cog import PlayerCog
 from .queue_cog import QueueCog
+from .report_cog import ReportsCog
+from .season_cog import SeasonCog
+from .settings_cog import SettingsCog
 from .stats_cog import StatsCog
